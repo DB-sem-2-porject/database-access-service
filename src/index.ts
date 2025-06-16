83a2160469ca039c2071eb2b6d25a8874d860d35
Plush-Jill <bockarevvlad23@gmail.com>
2025-06-16 19:27:48 +0700

    сделал резолвер для торговых точек

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,36 @@
-import {DatabaseAccessService} from './service.ts';
-import {readFileSync} from 'node:fs';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app-module.ts';
+import { DataSource } from 'typeorm';
+import {readFileSync} from "node:fs";
+import axios from 'axios';
+import * as Entities from "database-entity-service-lib";
+const allEntities = Object.values(Entities).filter(entity => typeof entity === 'function');
 
-async function index() {
+
+async function bootstrap() {
     let serviceConfigFileContent = readFileSync('./configs/service-config.json', 'utf8');
-    let dataBaseFileContent = readFileSync('./configs/database-config.json', 'utf8');
-
     let serviceConfig = JSON.parse(serviceConfigFileContent);
-    let dataBaseConfig = JSON.parse(dataBaseFileContent);
+    const port = serviceConfig.port;
+    try {
+        const response = await axios.get('http://localhost:40001/internal/data-source', {
+            headers: {'service-token': process.env.INTERNAL_SERVICE_TOKEN || 'default-token'},
+        });
+        const config = {
+            ...response.data,
+            entities: allEntities, // Добавляем entities локально
+        };
 
-    let service: DatabaseAccessService = await DatabaseAccessService.create({
-        port: serviceConfig.port,
-        host: serviceConfig.host,
-    });
+        const dataSource = new DataSource(config);
+        await dataSource.initialize();
+        console.log('DataSource initialized successfully');
+        const app = await NestFactory.create(AppModule.forRoot(dataSource));
+        await app.listen(port);
+        console.log('Database Access Service is running on port', port);
 
-    const result = await service.start();
-    console.log(result);
+    } catch (error) {
+        console.error('Failed to start application:', error);
+        process.exit(1);
+    }
 }
-
-index().then(result => {
-    console.log(result);
+bootstrap().then(result => {
 });
